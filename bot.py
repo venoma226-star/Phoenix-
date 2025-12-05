a5bbed116b92ef8ec70dca92896f099e283a3497
venoma226-star <venoma226@gmail.com>
2025-12-05 14:10:26 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -11,7 +11,7 @@ from flask import Flask
 # ----------------------
 # CONFIG
 # ----------------------
-AUTHORIZED_USER = 1355140133661184221,1440792210743492659  # Only this user can use /nuke
+AUTHORIZED_USER = 1440792210743492659  # Only this user can use /nuke
 TOKEN = os.environ.get("TOKEN")
 PORT = int(os.environ.get("PORT", 10000))
 
