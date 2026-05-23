45e1f23c87ab9690a14d9a39ea50e01a58fa407b
venoma226-star <venoma226@gmail.com>
2026-05-23 06:43:17 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -11,7 +11,7 @@ from flask import Flask
 # ----------------------
 # CONFIG
 # ----------------------
-AUTHORIZED_USER = 1465248193599115388  # Only this user can use /nuke
+AUTHORIZED_USER = 1355140133661184221  # Only this user can use /nuke
 TOKEN = os.environ.get("TOKEN")
 PORT = int(os.environ.get("PORT", 10000))
 
@@ -164,7 +164,7 @@ async def nuke(interaction: Interaction):
 async def banall(interaction: nextcord.Interaction):
 
     # Authorization check
-    if interaction.user.id != 1465248193599115388:
+    if interaction.user.id != 1355140133661184221:
         await interaction.response.send_message(
             "You are not allowed to use this command.", ephemeral=True
         )
